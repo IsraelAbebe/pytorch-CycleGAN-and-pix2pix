99f66cd75ae452cf29425bc7d722b9dfb3809f0e
icog <hmicheal50@gmail.com>
2019-08-14 07:48:26 +0000

    code for testing a single image is added, and default num_test is changed to 1

--- data/image_folder.py
+++ data/image_folder.py
@@ -22,13 +22,14 @@ def is_image_file(filename):
 
 def make_dataset(dir, max_dataset_size=float("inf")):
     images = []
-    assert os.path.isdir(dir), '%s is not a valid directory' % dir
-
-    for root, _, fnames in sorted(os.walk(dir)):
-        for fname in fnames:
-            if is_image_file(fname):
-                path = os.path.join(root, fname)
-                images.append(path)
+    if os.path.isdir(dir):
+        for root, _, fnames in sorted(os.walk(dir)):
+            for fname in fnames:
+                if is_image_file(fname):
+                    path = os.path.join(root, fname)
+                    images.append(path)
+    else:
+        images.append(dir)
     return images[:min(max_dataset_size, len(images))]
 
 
